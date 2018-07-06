4e09b26f225d5ea0c91ecf559ad33a5609f27985
sx14 <1059363093@qq.com>
2018-07-06 10:09:30 +0800

    modify

--- install.m
+++ install.m
@@ -5,9 +5,10 @@ end
 % Install own lib
 addpath(root);
 addpath(genpath(fullfile(root,'src')));
-% addpath(genpath(fullfile(root,'deepflow')));
 addpath(genpath(fullfile(root,'SLIC_mex')));
-addpath(genpath(fullfile(root,'PSPNet')));
-addpath(genpath(fullfile(root,'flownet2-master')));
+addpath(fullfile(root, 'PSPNet', 'evaluation'));
+addpath(fullfile(root, 'PSPNet'));
+% addpath(genpath(fullfile(root,'flownet2-master')));
+addpath(fullfile(root, 'flownet2-master'));
 args.video_path = '';
 args.output_path = '';
