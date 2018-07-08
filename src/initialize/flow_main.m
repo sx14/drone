b5274b218bd23354c006fe40c19efc7c71033872
sx14 <1059363093@qq.com>
2018-07-08 23:51:20 +0800

    flow fix

--- src/initialize/flow_main.m
+++ src/initialize/flow_main.m
@@ -1,17 +1,51 @@
-function flow_main(video_root, output_root, video_name)
+function flow_main(video_root, flow_output_root, resize_output_root, video_name)
 direction = 'forward'; % backward
-if ~exist(output_root, 'dir')
-    mkdir(output_root);
+if ~exist(flow_output_root, 'dir')
+    mkdir(flow_output_root);
 end
-if nargin==2
+if ~exist(resize_output_root, 'dir')
+    mkdir(resize_output_root);
+end
+if nargin==3
     videos = dir(fullfile(video_root,'*'));
     for i = 3:length(videos)
         video = videos(i);
-        cal_flow_fn(fullfile(video_root, video.name), fullfile(output_root, video.name), direction)
-%         cal_flow_match(video_root, output_root, video.name, direction);
+        [resize,width,height,org_width,org_height] = resize_scale(fullfile(video_root, video.name));
+        video_path = fullfile(video_root, video.name);
+        if resize == 1
+            resize_img(video_root,resize_output_root,video.name,'jpg',width,height,'bicubic');
+            video_path = fullfile(resize_output_root, video.name);
+        end
+        cal_flow_fn(video_path, fullfile(flow_output_root, video.name), direction);
+        if resize == 1
+            resize_img(resize_output_root,resize_output_root,video.name,'flo',org_width,org_height,'nearest');
+        end
     end
 end
-if nargin==3
-    cal_flow_fn(fullfile(video_root, video_name), fullfile(output_root, video_name), direction)
-%     cal_flow_match(video_root, output_root, video_name, direction);
+if nargin==4
+    [resize,width,height,org_width,org_height] = resize_scale(fullfile(video_root, video_name));
+    video_path = fullfile(video_root, video_name);
+    if resize == 1
+        resize_img(video_root,resize_output_root,video_name,'jpg',width,height,'bicubic');
+        video_path = fullfile(resize_output_root, video_name);
+    end
+    cal_flow_fn(video_path, fullfile(flow_output_root, video_name), direction);
+    if resize == 1
+        resize_img(resize_output_root,resize_output_root,video_name,'flo',org_width,org_height,'nearest');
+    end
 end
+
+function [resize,width,height,org_width,org_height] = resize_scale(video_path)
+img_path = fullfile(video_path, '0000001.jpg');
+img = imread(img_path);
+[org_height, org_width, ~] = size(img);
+max_length = 1300;
+scale = 1;
+resize = 0;
+max_org_length = max(org_height, org_width);
+if max_org_length > max_length
+    scale = max_length / max_org_length;
+    resize = 1;
+end
+width = floor(org_width * scale);
+height = floor(org_height * scale);
--- src/initialize/resize_img.m
+++ src/initialize/resize_img.m
@@ -1,34 +1,18 @@
-function [org_height, org_width, resized_imgs] = resize_img(video_package_path,output_path,video_dir)
-suffix = 'jpg';
+function resize_img(video_package_path,output_path,video_dir,suffix,width,height, method)
 video_path = fullfile(video_package_path,video_dir);
 video_resize_path = fullfile(output_path,video_dir);
 if ~exist(video_resize_path,'dir')
     mkdir(output_path, video_dir);
 end
 imgs = dir(fullfile(video_path, ['*.',suffix]));
-num1=num2str(1,'%07d');
-img_name = [num1,'.',suffix];
-I = imread(fullfile(video_path,img_name)); % orginal image
-org_height = size(I, 1);
-org_width = size(I, 2);
-resized_imgs = cell(length(imgs),1);
-default_length = 500;
 for i = 1:length(imgs)
     num1=num2str(i,'%07d');
     img_name = [num1,'.',suffix];
     resized_img_name = fullfile(video_resize_path,img_name);
     if ~exist(fullfile(video_resize_path, img_name),'file')
         I1 = imread(fullfile(video_path, img_name));
-        [max_length,~] = max([org_width,org_height]);
-        if max_length > default_length
-            scale =  default_length / max_length;
-            I1 = imresize(I1, scale);
-        end
+        I1 = imresize(I1, [height, width], method);
         imwrite(I1, resized_img_name);
-        resized_imgs{i+1} = I1;
-    else
-        I1 = imread(resized_img_name);
-        resized_imgs{i+1} = I1;
     end
 end
 disp('resize_img finished.');
